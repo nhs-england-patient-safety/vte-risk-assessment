5b58d32b83edaeb8c4cecb12b4c97309492ccc32
davidhardy5-nhs <112170082+davidhardy5-nhs@users.noreply.github.com>
2026-05-01 13:46:04 +0100

    create last day of quarter

--- R/functions.R
+++ R/functions.R
@@ -47,6 +47,7 @@ time_period_title <- case_when(
 )
 
 quarter <- as.numeric(substring(time_period, 6, 6))
+quarter_end_month <- c(6, 9, 12, 3)[quarter]
 time_period_paragraph <- time_period_title |> 
   str_replace("Quarter", str_to_lower) |> 
   str_replace("quarter [0-9]", str_glue("quarter {quarter} (Q{quarter})"))
@@ -60,6 +61,8 @@ month_2_readable <- as.character(format(make_date(year, month_1_q, 1) +
                                           months(1), "%B %Y"))
 month_3_readable <- as.character(format(make_date(year, month_1_q, 1) + 
                                           months(2), "%B %Y"))
+last_day_of_quarter <- make_date(year, quarter_end_month, 1) |> 
+  ceiling_date("month") - days(1)
 
 
 # figure saving
